ecca3ec1a350e6606dd14027f33e29d4ae226eb2
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-01-05 17:04:04 -0300

    Update acoes_petra4.py

--- acoes_petra4.py
+++ acoes_petra4.py
@@ -1,4 +1,13 @@
 import yfinance as yf
 from pandas import json_normalize
-import pandas as pandas
+import pandas as pd
+#criando ticker
 ticker = yf.Ticker('PBR')
+#cotação
+ticker.history(period='6d', interval='15m')
+# Balanço trimestral
+ticker.quarterly_balance_sheet.T
+#Dividendos
+ticker.dividends
+ticker.get_info()['preMarketPrice']
+df = pd.dataframe[ticker.history(period='6d', interval='15m')]
